1fcf4dfe844fde209c114d2c0b6fc4accc4a6fe1
Vilayat Kleer <vkleer@hotmail.com>
2022-10-03 01:03:50 +0000

    Create Sidebar function and import it in App.js

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+    return <div>
+        <a href="">My Photos</a>
+        <a href="">My Illustrations</a>
+        <a href="">My Paintings</a>
+    </div>
+}
+
+export default Sidebar;
