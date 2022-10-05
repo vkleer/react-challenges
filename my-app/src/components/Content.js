f131d19f426d1b9df10ab5221e42f9d9a857300c
Vilayat Kleer <vkleer@hotmail.com>
2022-10-05 22:31:55 +0000

    Create Loader component which displays a spinning loading icon, display Loader in Content component for 2 seconds after the Content has been mounted

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react'
 import {savedPosts} from '../posts.json'
 import css from './css/Content.module.css'
 import PostItem from './PostItem'
+import Loader from './Loader'
 
 export class Content extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            isLoaded: false,
+        };
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true,
+            })
+        }, 2000)
     }
 
     render() {
@@ -16,7 +28,13 @@ export class Content extends Component {
                     <h1>My Photos</h1>
                 </div>
                 <div className= {css.SearchResults}>
-                    <PostItem savedPosts={savedPosts} />
+                    {
+                        this.state.isLoaded ? (
+                            <PostItem savedPosts={savedPosts} />
+                        ) : (
+                            <Loader />
+                        )
+                    }  
                 </div>
             </div>
         )
